97e5ad755623fbf5139e61ec6f95125054602f69
student20 <student20@pcoip.ki.agh.edu.pl>
2018-02-25 15:38:01 +0100

    3 commit

---
+++ faktury/src/main/DrukujFakture.java
@@ -0,0 +1,25 @@
+package main;
+
+import java.util.Iterator;
+
+import dokumenty.Faktura;
+import dokumenty.Pozycja;
+
+public class DrukujFakture {
+
+	public void wypiszFakture(Faktura faktura)
+	{
+		System.out.println("=====================================================");
+		System.out.println("FA z dnia: "+faktura.getDataSprzedazy().toString());
+		System.out.println("Wystawiona dla: "+faktura.getKontrahent());
+		System.out.println("Na kwote: "+faktura.getSuma());
+		Iterator<Pozycja> iteratorPozycji=faktura.getIteratorPozycji();
+		while(iteratorPozycji.hasNext())
+		{
+			Pozycja pozycja=iteratorPozycji.next();
+			System.out.println("Towar: "+pozycja.getNazwa()+" Ilosc: "+pozycja.getIlosc()+" Wartosc:" + pozycja.getWartosc());
+		}
+		System.out.println("=====================================================");
+	}
+
+}
--- faktury/src/main/Wydrukfaktury.java
+++ faktury/src/main/Wydrukfaktury.java
@@ -1,25 +1,6 @@
 package main;
 
-import java.util.Iterator;
-
-import dokumenty.Faktura;
-import dokumenty.Pozycja;
 
 public class Wydrukfaktury {
-
-	public void wypiszFakture(Faktura faktura)
-	{
-		System.out.println("=====================================================");
-		System.out.println("FA z dnia: "+faktura.getDataSprzedazy().toString());
-		System.out.println("Wystawiona dla: "+faktura.getKontrahent());
-		System.out.println("Na kwote: "+faktura.getSuma());
-		Iterator<Pozycja> iteratorPozycji=faktura.getIteratorPozycji();
-		while(iteratorPozycji.hasNext())
-		{
-			Pozycja pozycja=iteratorPozycji.next();
-			System.out.println("Towar: "+pozycja.getNazwa()+" Ilosc: "+pozycja.getIlosc()+" Wartosc:" + pozycja.getWartosc());
-		}
-		System.out.println("=====================================================");
-	}
 
 }
